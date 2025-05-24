3df4af1688f42ceddcd1b4cd60ea75a20866b9cf
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-05-24 18:23:38 +0530

    basic progressbar complete

---
+++ progress-bar/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const ProgressBar = ({ progress }) => {
+  const [animtedProgress , setAnimtedProgress] = useState(0)
+  
+  useEffect(()=>{
+    setTimeout(() => 
+     setAnimtedProgress(progress)       
+    , 100);
+  },[progress])
+       
+   return (
+    <div className="outer">
+      <div
+        style={{
+          // width: `${animtedProgress}%`,
+          transform : `translateX(${animtedProgress-100}%)`,
+          color: animtedProgress < 5 ? "black" : "white",
+        }}
+        role="progressbar"
+        aria-valuenow={progress}
+        aria-valuemax='100'
+        aria-valuemin='0'
+        className="inner"
+      >
+        {progress}%
+      </div>
+    </div>
+  );
+};
+
+const bar = [0, 2, 5, 10, 30, 50, 70, 90, 100];
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Progress Bar</h1>
+      {bar.map((value,i) => (
+        <ProgressBar key={i} progress={value} />
+      ))}
+    </>
+  );
+}
+
+export default App;
