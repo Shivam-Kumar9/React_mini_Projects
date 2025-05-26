89c864124e062e616b76728421e40fa58eefc513
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-05-27 03:16:10 +0530

    debounce

--- HCJ-Daily-Planner/js/debounc.js
+++ HCJ-Daily-Planner/js/debounc.js
@@ -1,7 +1,29 @@
 //searh debounce
-export function debouncing(searchInput,searchBtn,render){
-
-  searchInput.addEventListener('change',()=>{})
+export function debouncing(searchInput, searchBtn, render) {
    
 
-} 
+   
+    const data = JSON.parse(localStorage.getItem("tasks")) || [];
+    
+     function debounce(fun,delay){
+        let timerid
+        return function(...arg){
+            timerid = setTimeout(()=>{
+                fun(...arg)
+            },delay)
+        }
+     }
+    
+     function main(value){
+        let serchedQ  = data.filter(ele=> ele.task.toLowerCase().includes(value.toLowerCase()) )
+        render(serchedQ)
+     } 
+     
+     let  linking = debounce(main,400)
+
+
+    searchInput.addEventListener("keyup", e => {
+      let query = e.target.value
+      linking(query)
+  });
+}
