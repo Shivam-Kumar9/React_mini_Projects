c255c57b2ad019e044a18fcffbe4e91709cfd231
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-09-23 00:06:20 +0530

    feat :  multi select

--- 21_miniProject/src/Components/accordian/index.jsx
+++ 21_miniProject/src/Components/accordian/index.jsx
@@ -4,19 +4,33 @@ import "./style.css";
 
 function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [multipleSelect, setMultipleSelected] = useState(false);
+  const [multiple, setMultiple ] = usestate([])
+  
   const handleSingleSelect = (currentid) => {
     console.log(currentid);
     setSelected(currentid === selected ? null : currentid);
   };
+
+  const handleMultiSelect = (id)=>{
+   const copyMultiple = [...multiple]
+   const isIndexOf = copyMultiple.indexOf(id)
+   if(!isIndexOf) copyMultiple.push(id)
+    else copyMultiple.splice(isIndexOf,1)
+   setMultiple(copyMultiple)
+  }
+
   return (
     <div className="wrapper">
-      <button>Enable multipe </button>
+      <button onClick={()=>setMultipleSelected(!multipleSelect)}>Enable multipe {multipleSelect ? "🟢" : "🔴"} </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div key={dataItem.id} className="item">
               <div
-                onClick={() => handleSingleSelect(dataItem.id)}
+                onClick={() => multipleSelect
+                  ?  handleMultiSelect(dataItem.id)
+                  :  handleSingleSelect(dataItem.id) }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
@@ -25,6 +39,10 @@ function Accordian() {
               {selected === dataItem.id ? (
                 <div className="content">{dataItem.answer}</div>
               ) : null}
+              {/* {multipleSelect 
+                ? 
+        
+              } */}
             </div>
           ))
         ) : (
