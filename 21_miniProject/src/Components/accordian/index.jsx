ed9c6f411664855b493963d9dc450d72f8537b09
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-09-23 23:12:04 +0530

    fix: improve multi-select functionality and clean up code

--- 21_miniProject/src/Components/accordian/index.jsx
+++ 21_miniProject/src/Components/accordian/index.jsx
@@ -5,44 +5,60 @@ import "./style.css";
 function Accordian() {
   const [selected, setSelected] = useState(null);
   const [multipleSelect, setMultipleSelected] = useState(false);
-  const [multiple, setMultiple ] = usestate([])
-  
+  const [multiple, setMultiple] = useState([]);
+
   const handleSingleSelect = (currentid) => {
-    console.log(currentid);
+    // console.log(currentid);
     setSelected(currentid === selected ? null : currentid);
   };
 
-  const handleMultiSelect = (id)=>{
-   const copyMultiple = [...multiple]
-   const isIndexOf = copyMultiple.indexOf(id)
-   if(!isIndexOf) copyMultiple.push(id)
-    else copyMultiple.splice(isIndexOf,1)
-   setMultiple(copyMultiple)
-  }
+  const handleMultiSelect = (id) => {
+    const copyMultiple = [...multiple];
+    const isIndexOf = copyMultiple.indexOf(id);
+
+    console.log(isIndexOf, typeof id, id);
+    if (isIndexOf === -1) copyMultiple.push(id);
+    else copyMultiple.splice(isIndexOf, 1);
+    console.log(copyMultiple);
+    setMultiple(copyMultiple);
+  };
 
   return (
     <div className="wrapper">
-      <button onClick={()=>setMultipleSelected(!multipleSelect)}>Enable multipe {multipleSelect ? "🟢" : "🔴"} </button>
+      <button onClick={() => setMultipleSelected(!multipleSelect)}>
+        Enable multipe {multipleSelect ? "🟢" : "🔴"}{" "}
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div key={dataItem.id} className="item">
               <div
-                onClick={() => multipleSelect
-                  ?  handleMultiSelect(dataItem.id)
-                  :  handleSingleSelect(dataItem.id) }
+                onClick={() =>
+                  multipleSelect
+                    ? handleMultiSelect(dataItem.id)
+                    : handleSingleSelect(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
+              {
+                multipleSelect
+                ? multiple.includes(dataItem.id) && (
+                    <div className="content">{dataItem.answer}</div>
+                  ) 
+                : (selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>))
+
+              }
+
+              {/* {selected === dataItem.id || multiple.includes(dataItem.id) ? (
                 <div className="content">{dataItem.answer}</div>
-              ) : null}
-              {/* {multipleSelect 
-                ? 
-        
-              } */}
+              ) : null}  
+                this is good but this create bugg when we select multiple and then switch to single
+               */} 
+              
             </div>
           ))
         ) : (
