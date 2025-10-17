69766c47c86a630b3ab18b085750891d0b3e46ad
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-10-17 23:52:02 +0530

    feat: implement image fetching and loading/error handling in ImageSlider component

--- 21_miniProject/src/Components/image-slider/index.jsx
+++ 21_miniProject/src/Components/image-slider/index.jsx
@@ -1,8 +1,38 @@
+import { useState } from 'react'
 import './style.css'
 
 
 
-export default function ImageSlider({url , limit}){
-
+export default function ImageSlider({url ,  limit}){
+   
+    const [image,setImage]= useState([])
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [error,setError] = useState(null)
+    const [loading, setLoading] =useState(false)
     
+
+   async function fetchImage(getUrl) {
+      try {
+          setLoading(true)
+        const response = await fetch(getUrl)
+        const data     = await response.json();
+        
+        if(data){
+            setImage(data)
+        }
+      } catch (error) {
+        setError(error)
+      }
+        finally{
+            setLoading(false)
+        }
+   }
+
+   if (loading) {(<h1>Loading...</h1>)}
+  
+   if (error !== null ){
+     return <div>Error ocurred !{error}</div>
+   } 
+
+    return <div className='container'></div>
 }
