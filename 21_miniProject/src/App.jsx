73c121a3609589500c2b34d20165cc6777eae5b3
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-11-11 22:43:29 +0530

    feat: add ImageSlider component with image fetching, loading, and error handling

--- 21_miniProject/src/App.jsx
+++ 21_miniProject/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Accordian from './Components/accordian'
 import RandomColor from './Components/random_color'
 import StarRating from './Components/star_rating'
+import ImageSlider from './Components/image-slider'
 
 function App() {
  
@@ -16,7 +17,10 @@ function App() {
        {/* <RandomColor/> */}
 
        {/* rating star */}
-       <StarRating/>
+       {/* <StarRating/> */}
+
+      {/* Image slider component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"}/>
     </>
   )
 }
--- 21_miniProject/src/Components/image-slider/index.jsx
+++ 21_miniProject/src/Components/image-slider/index.jsx
@@ -1,38 +1,63 @@
-import { useState } from 'react'
-import './style.css'
+import { useEffect, useState } from "react";
+import "./style.css";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
+export default function ImageSlider({ url, limit }) {
+  const [image, setImage] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  async function fetchImage(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=1&limit=${limit}`);
+      const data = await response.json();
 
-export default function ImageSlider({url ,  limit}){
-   
-    const [image,setImage]= useState([])
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [error,setError] = useState(null)
-    const [loading, setLoading] =useState(false)
-    
-
-   async function fetchImage(getUrl) {
-      try {
-          setLoading(true)
-        const response = await fetch(getUrl)
-        const data     = await response.json();
-        
-        if(data){
-            setImage(data)
-        }
-      } catch (error) {
-        setError(error)
+      if (data) {
+        setImage(data);
       }
-        finally{
-            setLoading(false)
-        }
-   }
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-   if (loading) {(<h1>Loading...</h1>)}
-  
-   if (error !== null ){
-     return <div>Error ocurred !{error}</div>
-   } 
+  useEffect(() => {
+    if (url !== "") fetchImage(url);
+  }, [url]);
 
-    return <div className='container'></div>
+  if (loading) {
+    return <h1>Loading data ! Please wait</h1>;
+  }
+
+  if (error !== null) {
+    return <div>Error ocurred !{error}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow_left" />
+      {image.length && image
+        ? image.map((img, i) => (
+            <img
+              src={img.download_url}
+              alt="img"
+              key={i}
+              className="current_image"
+            />
+          ))
+        : null}
+
+      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <span className="circle-indicators">
+        {image && image.length
+          ? image.map((_, index) => (
+              <button key={index} className="current_indicator">_</button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
