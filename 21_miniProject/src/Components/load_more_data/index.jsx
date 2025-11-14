1f9f1539955876e05a5d927cafdc1554867922ab
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-11-14 21:28:35 +0530

    feat: update LoadMoreData component to fetch 20 products and disable button when all products are loaded

--- 21_miniProject/src/Components/load_more_data/index.jsx
+++ 21_miniProject/src/Components/load_more_data/index.jsx
@@ -5,16 +5,17 @@ export default function LoadMoreData() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
   async function fetchProducts() {
     try {
       setLoading(true);
       const response = await fetch(
-        `https://dummyjson.com/products?limit=10&skip=${
-          count === 0 ? 0 : count * 10
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
         }`
       );
-
+        
       const result = await response.json();
       if (result && result.products && result.products.length) {
         setProducts(prevData => [...prevData, ...result.products]);
@@ -31,6 +32,10 @@ export default function LoadMoreData() {
   useEffect(() => {
     fetchProducts();
   }, [count]);
+
+  useEffect(()=>{
+    if(products && products.length === 100) setDisableButton(true)
+  },[products])
 
   if (loading) {
     return <h1>Loading data ! Please wait</h1>;
@@ -51,9 +56,14 @@ export default function LoadMoreData() {
 
       <div className="btn_container">
         <button 
+        disabled={disableButton}
         onClick={()=>setCount(count+1)}
         className="load_more_button"
-        >Load More</button>
+        >Load More{count}
+        </button>
+        {
+          disableButton ? <p>All products are loaded</p> : null
+        }
       </div>
     </div>
   );
