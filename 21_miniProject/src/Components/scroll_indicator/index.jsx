27ed5c3296bdc1b5aab56f98f4c647b308687972
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-12-18 23:03:43 +0530

    feat: enhance ScrollIndicator with data fetching and add dummy data

--- 21_miniProject/src/Components/scroll_indicator/index.jsx
+++ 21_miniProject/src/Components/scroll_indicator/index.jsx
@@ -1,33 +1,54 @@
 import './scroll.css'
 import React, { useState, useEffect } from 'react';
-
-export default function ScrollIndicator({url}) {
+import fake from './deta';
+export default function ScrollIndicator({url}){
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
-     
+    const [howMuchScrollPercent, setHowMuchScrollPercent] = useState(0)
     async function fetchData(geturl){
-        setLoading(true) 
+        setLoading(true)
        try{
           const response = await fetch(geturl)
           const data = await response.json()
-          console.log(data.products);
+          console.log(data.products)
            if( data && data.products && data.products.length >0) setData(data.products)
        } catch (e) {
-        console.log(e);
+        console.log('hi',e);
         setError(e.message)
        }
       finally { setLoading(false)}
     }
-     useEffect(()=>fetchData(url),[url])    
-    
+
+    const handleScroll =()=>{
+        // console.log("scrollTop",document.documentElement.scrollTop,
+        //              document.body.scrollTop,
+        //         "scrollHeight",document.documentElement.scrollHeight,
+        //         "custom",document.documentElement.clientHeight)
+         const actualHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        // clintHeight -> height of the view port  ---- scrollHeight --total scroll content on web
+         const howMuchScrolled = document.documentElement.scrollTop || document.body.scrollTop
+         setHowMuchScrollPercent((actualHeight/howMuchScrolled)*100)
+     }
+     useEffect(()=>{fetchData(url)},[url]) 
+     useEffect(()=>{
+       window.addEventListener('scroll', handleScroll)
+       
+       return ()=>{window.removeEventListener('scroll',()=>{handleScroll()})}
+     },[])
+     //Q why many times run if dependency one
+     //Jaise aapne ek baar (useEffect) kisi ko bell (EventListener) de di aur kaha ki "Jab bhi koi aaye, bell baja dena". Ab bhale hi aapne bell sirf ek baar di, lekin wo bajegi to har baar jab koi aayega!
+
     return (
         <div className="">
             <h1>Custom Scroll indicator</h1>
-            <div className="data_container" >
-                {data && data.length >0 ? data.map(dataItem => <p>{dataItem.title}</p> 
+            <div className="data_container">
+                {data && data.length >0 ? data.map(dataItem => <p key={dataItem.id}>{dataItem.title}</p> 
                                        ): null }
+                 { /* {dummy data} */
+                  //  fake.map((ele,i)=><p key={i}>{ele.title}</p>) 
+                  }
             </div>
-        </div>
+         </div>
     )
 }
