b6be43707a9bfc09c693fc0161f36ba6399439bc
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2026-04-15 19:43:27 +0530

    feat: refactor TransactionForm to use Dialog component for improved UX

--- 3.Expense-Tracker/src/components/add-transaction/index.jsx
+++ 3.Expense-Tracker/src/components/add-transaction/index.jsx
@@ -1,55 +1,133 @@
 import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
+  Dialog,
+  Button,
+  Input,
   RadioGroup,
 } from "@chakra-ui/react";
 
-export default function TrasactionForm() {
+export default function TransactionForm({ onClose, isOpen }) {
   return (
-    <Modal>
-      <form>
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader>Add New Transaction</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <FormControl>
-                <FormLabel>Enter Description</FormLabel>
-                <Input
-                  placeholder="Enter Transaction Description"
-                  name="description"
-                  type="text"
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Enter Description</FormLabel>
-                <Input
-                  placeholder="Enter Transaction Description"
-                  name="description"
-                  type="text"
-                />
-              </FormControl>
-                            <FormControl>
-                <FormLabel>Enter  Amount</FormLabel>
-                <Input 
-                  placeholder="Enter Transaction Description"
-                  name="amount"
-                  type="number"
-                />
-              </FormControl>
-              <RadioGroup mt='5'>
-                 <Radio value='income' colorScheme='blue' name='type'>Income</Radio>
-                 <Radio value='expense' colorScheme='red'  name='type'>Expense</Radio>
-              </RadioGroup>
-            </ModalBody>
-          </ModalContent>
-        </ModalOverlay>
-      </form>
-    </Modal>
+    <Dialog.Root
+      open={isOpen}
+      onOpenChange={(details) => {
+        if (!details.open) onClose();
+      }}
+    >
+      <Dialog.Backdrop />
+
+      <Dialog.Content>
+        <form>
+          <Dialog.Header>Add New Transaction</Dialog.Header>
+          <Dialog.CloseTrigger />
+
+          <Dialog.Body>
+            <div>
+              <label>Description</label>
+              <Input name="description" />
+            </div>
+
+            <div>
+              <label>Amount</label>
+              <Input name="amount" type="number" />
+            </div>
+
+            <RadioGroup defaultValue="income">
+              <RadioGroup.Item value="income">
+                <RadioGroup.ItemHiddenInput />
+                <RadioGroup.ItemIndicator />
+                <RadioGroup.ItemText>Income</RadioGroup.ItemText>
+              </RadioGroup.Item>
+
+              <RadioGroup.Item value="expense">
+                <RadioGroup.ItemHiddenInput />
+                <RadioGroup.ItemIndicator />
+                <RadioGroup.ItemText>Expense</RadioGroup.ItemText>
+              </RadioGroup.Item>
+            </RadioGroup>
+          </Dialog.Body>
+
+          <Dialog.Footer>
+            <Button onClick={onClose} type="button">
+              Cancel
+            </Button>
+            <Button type="button">Add</Button>
+          </Dialog.Footer>
+        </form>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 }
+
+
+
+// import {
+//    DialogBackdrop,
+//   DialogContent,
+//   DialogHeader,
+//   DialogFooter,
+//   DialogBody,
+//   DialogCloseTrigger,
+//    Button,
+ 
+//   Input,
+// } from "@chakra-ui/react";
+// import {
+//   DialogRoot as Dialog,
+//   RadioGroup,
+//    Field,
+//  } from "@chakra-ui/react";
+
+// export default function TransactionForm({ onClose, isOpen }) {
+//   return (
+//     <Dialog open={isOpen} onOpenChange={(details) => {
+//   if (!details.open) onClose();
+// }}>
+//         <DialogBackdrop/>  
+//         <DialogContent>
+//       <form>
+//           <DialogHeader>Add New Transaction</DialogHeader>
+//           <DialogCloseTrigger />
+//           <DialogBody>
+//             <Field>
+//               <Field.Label>Enter Description</Field.Label>
+//               <Input
+//                 placeholder="Enter Transaction Description"
+//                 name="description"
+//                 type="text"
+//               />
+//             </Field>
+ 
+//             <Field>
+//               <Field.Label>Enter Amount</Field.Label>
+//               <Input
+//                 placeholder="Enter Transaction Amount"
+//                 name="amount"
+//                 type="number"
+//               />
+//             </Field>
+//             <RadioGroup mr={'4'} defaultValue="income">
+//               <RadioGroup.Item value="income">
+//                 <RadioGroup.ItemHiddenInput />
+//                 <RadioGroup.ItemIndicator />
+//                 <RadioGroup.ItemText>Income</RadioGroup.ItemText>
+//               </RadioGroup.Item>
+
+//               <RadioGroup.Item value="expense">
+//                 <RadioGroup.ItemHiddenInput />
+//                 <RadioGroup.ItemIndicator />
+//                 <RadioGroup.ItemText>Expense</RadioGroup.ItemText>
+//               </RadioGroup.Item>
+//             </RadioGroup>
+//           </DialogBody>
+//           <DialogFooter>
+//             <Button mr={"4"} onClick={onClose}>
+//               Cancel
+//             </Button>
+//             <Button type="button">Add</Button>
+//           </DialogFooter>
+//       </form>
+//         </DialogContent>
+//     </Dialog>
+ 
+//   );
+// }
